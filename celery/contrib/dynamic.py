9887d498f6213923458999db17b3aa43274b17b4
Steeve Morin <steeve.morin@gmail.com>
2012-06-21 21:00:45 +0200

    Actually replace the @task operator with @dynamic_task

--- celery/contrib/dynamic.py
+++ celery/contrib/dynamic.py
@@ -17,18 +17,15 @@ Usage example
 
 .. code-block:: python
 
-    from celery import task
     from celery.contrib.dynamic import dynamic_task
 
-    @task
     @dynamic_task
     def one(i):
         if i > 3:
             return two.s(i)
         return i + 1
 
-    @task
-    @dynamic_task
+    @dynamic_task(ignore_result=True)
     def two(i):
         return i + 2
 
@@ -39,25 +36,31 @@ Usage example
 """
 from __future__ import absolute_import
 
-from celery import current_task
+from celery import current_task, task
 from celery.canvas import Signature
 from functools import wraps
 
 
-def dynamic_task(fn):
-    @wraps(fn)
-    def _wrap(*args, **kwargs):
-        retval = fn(*args, **kwargs)
-        if isinstance(retval, Signature):
-            retval.immutable = True
-            if current_task.request.callbacks:
-                retval |= current_task.request.callbacks[0]
-            if current_task.request.chord:
-                retval.set(chord=current_task.request.chord)
-                current_task.request.chord = None
-            if current_task.request.group:
-                retval.set(group_id=current_task.request.group)
-                current_task.request.group = None
-            current_task.request.callbacks = [retval]
-        return retval
-    return _wrap
+def dynamic_task(*args, **kwargs):
+    def do_wrap(fn):
+        @wraps(fn)
+        def _fn_wrapper(*args, **kwargs):
+            retval = fn(*args, **kwargs)
+            if isinstance(retval, Signature):
+                retval.immutable = True
+                if current_task.request.callbacks:
+                    retval |= current_task.request.callbacks[0]
+                if current_task.request.chord:
+                    retval.set(chord=current_task.request.chord)
+                    current_task.request.chord = None
+                if current_task.request.group:
+                    retval.set(group_id=current_task.request.group)
+                    current_task.request.group = None
+                current_task.request.callbacks = [retval]
+            return retval
+        return task(*args, **kwargs)(_fn_wrapper)
+    if len(args) == 1 and callable(args[0]):
+        fn = args[0]
+        args = args[1:]
+        return do_wrap(fn)
+    return do_wrap
